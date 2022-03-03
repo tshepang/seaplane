b5f089d0fbe068c4adc39a8756a47e7b4bed5f35
Kevin K <kevin@seaplane.io>
2022-03-02 19:35:04 -0500

    init: adds --overwrite={all,config,formations,flights} and bug fixes (#21)
    This PR addresses those by fix the papercut bugs and adds --overwrite=ITEM where the item can be any of all,config,formations,flights. --force is effectively the same as --overwrite=all.
    
    There is also the minor change for the better now that it's possible to suggest the least destructive option when a file already exists.
    
    Co-authored-by: Eric Fang <github@accounts.unkies.org>

--- seaplane-cli/src/cli/cmds/init.rs
+++ seaplane-cli/src/cli/cmds/init.rs
@@ -2,14 +2,26 @@ use std::fs;
 
 use clap::Parser;
 
-use crate::{context::Ctx, error::Result};
+use crate::{context::Ctx, error::Result, fs::conf_dirs};
 
 /// Create the Seaplane directory structure at the appropriate locations
 #[derive(Parser)]
 pub struct SeaplaneInitArgs {
     /// Force create the files and directories (DANGER: will overwrite existing files)
-    #[clap(long)]
+    #[clap(
+        long,
+        long_help = "Force create the files and directories (DANGER: will overwrite existing files)
+
+Using --force is the same as using --overwrite=all"
+    )]
     pub force: bool,
+
+    /// Overwrite select files or directories (DANGER: will overwrite existing data)
+    #[clap(long, possible_values = &["all", "formations", "flights", "config"],
+        long_help = "Overwrite select files or directories (DANGER: will overwrite existing data)
+
+Using --overwrite=all is the same as using --force")]
+    pub overwrite: Option<String>,
 }
 
 impl SeaplaneInitArgs {
@@ -20,24 +32,40 @@ impl SeaplaneInitArgs {
         cli_debugln!("Creating directory {:?}", ctx.data_dir());
         fs::create_dir_all(ctx.data_dir())?;
 
-        let to_create = &[(ctx.formations_file(), b"{}"), (ctx.flights_file(), b"[]")];
-        for (file, empty_bytes) in to_create {
+        // We only creat the first (most preferred) configuration dir. If the user creates more
+        // down our search path, that's fine, but we only create and advertise the first.
+        let conf_dir = &conf_dirs()[0];
+        cli_debugln!("Creating directory {:?}", conf_dir);
+        fs::create_dir_all(conf_dir)?;
+
+        // Tuple below is: (File, "empty" bytes, it's --force=OPTION)
+        let to_create = &[
+            (conf_dir.join("seaplane.toml"), "[account]\n", "config"),
+            (ctx.formations_file(), "{}", "formations"),
+            (ctx.flights_file(), "[]", "flights"),
+        ];
+        for (file, empty_bytes, opt) in to_create {
             if file.exists() {
-                if ctx.force {
-                    cli_warn!(@Yellow, "warn: ");
-                    cli_warn!("overwriting existing file ");
-                    cli_warn!(@Green, "{:?} ", file);
-                    cli_warn!("due to '");
-                    cli_warn!(@Green, "--force");
-                    cli_warnln!(@noprefix, "'");
-                } else {
-                    cli_warn!(@Yellow, "warn: ");
-                    cli_warn!(@Green, "{:?} ", file);
-                    cli_warnln!(@noprefix, "already exists");
-                    cli_warn!("(hint: use '");
-                    cli_warn!(@Green, "seaplane init --force");
-                    cli_warnln!(@noprefix, "to erase and overwrite it)");
-                    continue;
+                match (ctx.force, &self.overwrite) {
+                    (true, Some(val)) | (_, Some(val)) if val == opt || val == "all" => {
+                        cli_warn!(@Yellow, "warn: ");
+                        cli_warn!("overwriting existing file ");
+                        cli_warn!("{:?} ", file);
+                        cli_warn!("due to '");
+                        cli_warn!(@Green, "{}", if ctx.force { "--force".into() } else { format!("--overwrite={}", val)});
+                        cli_warnln!(@noprefix, "'\n");
+                    }
+                    _ => {
+                        // We only want to advertise the *least* destructive option, not --force or
+                        // --ovewrite=all. The user can find those on their own.
+                        cli_warn!(@Yellow, "warn: ");
+                        cli_warn!("{:?} ", file);
+                        cli_warnln!(@noprefix, "already exists");
+                        cli_warn!("(hint: use '");
+                        cli_warn!(@Green, "seaplane init --overwite={} ", opt);
+                        cli_warnln!(@noprefix, "to erase and overwrite it)\n");
+                        continue;
+                    }
                 }
             }
             cli_debugln!("creating file {:?}", file);
